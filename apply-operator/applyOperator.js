44929153422f34ebdd2f90f6d29104ab890453a2
Mriyam Tamuli <mbtamuli@gmail.com>
2019-08-21 17:10:01 +0530

    WIP: Code is not running
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,6 +1,19 @@
-
 function applyOperator(...args) {
-  return args;
+  let expr;
+  if (args.length > 1) {
+    if ((typeof args[1] === 'string') && ('+-*/%'.indexOf(args[0]) !== -1)) {
+      // eslint-disable-next-line
+      expr = args.forEach(function(value, index, arr) {
+        if (index !== 0) {
+          expr += value + (index < arr.length - 1 ? arr[0] : '');
+        }
+      });
+      // eslint-disable-next-line
+      return eval(expr);
+    }
+    throw new Error('Specify operator as string');
+  }
+  throw new Error('Operator not specified');
 }
 
 export {
