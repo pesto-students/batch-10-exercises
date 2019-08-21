e707bbbf86f6e43c67c0955dd0daa5f32012db95
hacknaman <hacknaman@gmail.com>
2019-08-21 16:49:14 +0530

    done factorial

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,6 +1,13 @@
 
+function myFactorial(val) {
+  if (val == 0 || val == 1 ) 
+    return 1;
+
+  return val * myFactorial(val-1);
+} 
+
 function factorial(...args) {
-  return args;
+  return myFactorial(args[0]);
 }
 
 export {
