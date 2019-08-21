db2d05193a8e8e190a274d79e8de2f95f82a616a
somaran <sarathsomana@gmail.com>
2019-08-21 16:37:01 +0530

    Solves factorial

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,6 +1,13 @@
 
 function factorial(...args) {
-  return args;
+  let result = 1;
+  while (args > 1) {
+    result *= args;
+    // eslint-disable-next-line no-plusplus
+    // eslint-disable-next-line no-param-reassign
+    args -= 1;
+  }
+  return result;
 }
 
 export {
