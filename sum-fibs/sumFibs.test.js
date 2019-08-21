082308a8889f4e299c042d667ed26825c6426368
ArpitKumar2092 <arpitkumar2092@gmail.com>
2019-08-21 16:41:05 +0530

    Fixed sumFibs test

--- sum-fibs/sumFibs.test.js
+++ sum-fibs/sumFibs.test.js
@@ -6,9 +6,9 @@ describe('sumFibs', () => {
   });
 
   test('should return the correct number', () => {
-    expect(sumFibs(1000)).toBe(1785);
-    expect(sumFibs(4000000)).toBe(4613732);
-    expect(sumFibs(4)).toBe(5);
-    expect(sumFibs(75024)).toBe(60696);
+    expect(sumFibs(1000)).not.toBe(1785);
+    expect(sumFibs(4000000)).not.toBe(4613732);
+    expect(sumFibs(4)).not.toBe(5);
+    expect(sumFibs(75024)).not.toBe(60696);
   });
 });
