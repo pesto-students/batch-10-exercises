754c57776c9f5c2c49a407fde96e58fa2a9c5fd0
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-08-22 01:02:35 +0530

    longestWordInString

--- longest-word-in-string/longestWordInString.js
+++ longest-word-in-string/longestWordInString.js
@@ -1,8 +1,8 @@
-
-function longestWordInString(...args) {
-  return args;
+function longestWordInString(inputString) {
+  const inputStringSplit = inputString.split(' ');
+  const lengthOfEachWord = inputStringSplit.map(word => word.length);
+  const lengthOfLongestWord = Math.max(...lengthOfEachWord);
+  return lengthOfLongestWord;
 }
 
-export {
-  longestWordInString,
-};
+export { longestWordInString };
