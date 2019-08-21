d32ee2a7e5911837d5fa59b22e0f1eeed37e379a
p-racoon <prasham9.ash@gmail.com>
2019-08-21 20:12:24 +0530

    excercise time ends, working on Palindrome

--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,6 +1,24 @@
 
-function isPalindrome(...args) {
-  return args;
+function isPalindrome(inputString) {
+  inputString = inputString.toLowerCase();
+  let cleanedString = removeNonAlphabets(inputString);
+
+  let reversedString = cleanedString.split('').reverse().join('');
+  return reversedString === cleanedString;
+  function removeNonAlphabets(inputString) {
+    let cleanedString = "";
+    [...inputString].map((eachChar) => {
+      cleanedString += isAlphabet(eachChar) ? eachChar : "";
+    })
+    return cleanedString;
+  }
+  function isAlphabet(inputCharacter) {
+    let alphabetRegex = /^[a-zA-Z0-9]+$/;
+    if (inputCharacter.match(alphabetRegex)) {
+      return true;
+    }
+    return false;
+  }
 }
 
 export {
