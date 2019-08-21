8abf735bb723fd3066697996fa9d30246dedbedf
somaran <sarathsomana@gmail.com>
2019-08-22 01:07:23 +0530

    Solves largeoffour

--- large-of-four/largeOfFour.js
+++ large-of-four/largeOfFour.js
@@ -1,6 +1,7 @@
 
-function largeOfFour(...args) {
-  return args;
+function largeOfFour(arrays) {
+  const maxReducer = (max, element) => (max > element ? max : element);
+  return arrays.map((array) => array.reduce(maxReducer));
 }
 
 export {
