63a000361486073887c52caca0b1338d8ddaa968
ArpitKumar2092 <arpitkumar2092@gmail.com>
2019-08-21 20:11:36 +0530

    Extended binary-anget test case

--- binary-agent/binaryAgent.js
+++ binary-agent/binaryAgent.js
@@ -1,5 +1,8 @@
 
 function binaryAgent(binaryString) {
+  if(typeof binaryString !== "string"){
+    throw `String was excepted but ${typeof binaryAgent} was supplied `;
+  }
   return binaryString.split(" ").map( function( binaryStringBlock ) {
     return String.fromCharCode( parseInt( binaryStringBlock, 2) );
   }).join("")
--- reverse-string/reverseString.js
+++ reverse-string/reverseString.js
@@ -1,5 +1,8 @@
 
 function reverseString(string) {
+  if(typeof string !== "string"){
+    throw `String was excepted but ${typeof binaryAgent} was supplied `;
+  }
   return string.split("").reverse().join("");
 }
 
