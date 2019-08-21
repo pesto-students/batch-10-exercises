e37de3299bbec00e7edd4c3f00e730cdabd77a9c
somaran <sarathsomana@gmail.com>
2019-08-21 20:28:38 +0530

    Solves rot13

--- rot13/rot13.js
+++ rot13/rot13.js
@@ -1,6 +1,16 @@
 
-function rot13(...args) {
-  return args;
+function rot13(string) {
+  const lowerCaseAlphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  const upperCaseAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  const rotate = (character) => {
+    if (lowerCaseAlphabet.includes(character)) {
+      return lowerCaseAlphabet[(lowerCaseAlphabet.indexOf(character) + 13) % 26];
+    } if (upperCaseAlphabet.includes(character)) {
+      return upperCaseAlphabet[(upperCaseAlphabet.indexOf(character) + 13) % 26];
+    }
+    return character;
+  };
+  return string.split('').map((character) => rotate(character)).join('');
 }
 
 export {
