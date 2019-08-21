f57a245da31493d111157afd93339d9f1d5b6e49
p-racoon <prasham9.ash@gmail.com>
2019-08-21 17:23:42 +0530

    apply operator in progress

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,6 +1,21 @@
 
 function applyOperator(...args) {
-  return args; 
+  if (['+', '-', '*', '/', '%'].indexOf(args[0]) < 0 || args.length == 0) {
+    throw new Error("No Operand Present");
+  }
+  else if (args.length === 1 && ['+', '-', '*', '/', '%'].indexOf(args[0]) >= 0) {
+    return 0;
+  }
+  else if (['+', '-', '*', '/', '%'].indexOf(args[0]) >= 0) {
+    switch (args[0]) {
+      case '+': return args.slice[1].reduce((sum, num) => sum + num)
+      case '-': return args.slice[1].reduce((result, num) => result - num)
+      case '*': return args.slice[1].reduce((result, num) => result * num)
+      case '/': return args.slice[1].reduce((result, num) => result / num)
+      case '%': return args.slice[1].reduce((result, num) => result % num)
+    }
+
+  }
 }
 
 export {
