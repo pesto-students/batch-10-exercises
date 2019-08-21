88d60ce13c940c13fbe0235a11ae6499e2d70d76
Ankeeth Suvarna <ankitsuvarna11@gmail.com>
2019-08-21 20:06:19 +0530

    completed telephone-check

--- telephone-check/telephoneCheck.js
+++ telephone-check/telephoneCheck.js
@@ -1,8 +1,8 @@
+function telephoneCheck(phoneNumber) {
+  const validPhoneFormatRegex = /^(1 )?(\([0-9]{3}\)\s?|[0-9]{3}-)[0-9]{3}-[0-9]{4}$/ig;
 
-function telephoneCheck(...args) {
-  return args;
+  const isValid = validPhoneFormatRegex.test(phoneNumber);
+  return isValid;
 }
 
-export {
-  telephoneCheck,
-};
+export { telephoneCheck };
