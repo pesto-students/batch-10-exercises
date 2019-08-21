43cae3bc9835c0bca062311d48b59ed3002f9a48
Ankeeth Suvarna <ankitsuvarna11@gmail.com>
2019-08-21 19:32:35 +0530

    completed sum-all

--- sum-all/sumAll.js
+++ sum-all/sumAll.js
@@ -1,8 +1,18 @@
-
-function sumAll(...args) {
-  return args;
+function sortNumbers(a, b) {
+  return a - b;
 }
 
-export {
-  sumAll,
-};
+function sumAll(numberArray) {
+  const [firstNumber, lastNumber] = numberArray.sort(sortNumbers);
+  let result = 0;
+  let count = firstNumber;
+
+  while (count <= lastNumber) {
+    result += count;
+    count += 1;
+  }
+
+  return result;
+}
+
+export { sumAll };
