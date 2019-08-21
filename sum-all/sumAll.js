52f33a8ffbd6b7389c56d16709b6d14cdb385cae
somaran <sarathsomana@gmail.com>
2019-08-22 00:03:06 +0530

    Solves sum all

--- sum-all/sumAll.js
+++ sum-all/sumAll.js
@@ -1,6 +1,12 @@
 
-function sumAll(...args) {
-  return args;
+function sumAll(numbers) {
+  // Sum of numbers within range can be calculated using
+  // difference between their running sum.
+  // using sum of numbers upto n (including n) = n*(n+1) / 2
+  const [smallNumber, bigNumber] = numbers.sort((a, b) => (a - b));
+  // eslint-disable-next-line no-mixed-operators
+  const sumTill = (number) => number * (number + 1) / 2;
+  return sumTill(bigNumber) - sumTill(smallNumber - 1);
 }
 
 export {
