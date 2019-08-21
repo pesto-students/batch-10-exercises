1f7ccb0dc0a0598d4d56e2ec9b29916013bbdbf5
somaran <sarathsomana@gmail.com>
2019-08-21 19:54:50 +0530

    Solves repeat string num times

--- repeat-string-num-times/repeatStringNumTimes.js
+++ repeat-string-num-times/repeatStringNumTimes.js
@@ -1,6 +1,7 @@
 
-function repeatStringNumTimes(...args) {
-  return args;
+function repeatStringNumTimes(string, number) {
+  const defaultString = '';
+  return number > 0 ? string.repeat(number) : defaultString;
 }
 
 export {
