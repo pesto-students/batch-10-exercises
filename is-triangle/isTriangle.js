a0a93919a721e894c26763a16eacf8afd520c51d
somaran <sarathsomana@gmail.com>
2019-08-21 17:08:13 +0530

    Solves is-triangle

--- is-triangle/isTriangle.js
+++ is-triangle/isTriangle.js
@@ -1,6 +1,8 @@
 
-function isTriangle(...args) {
-  return args;
+function isTriangle(...sides) {
+  return ((sides[0] + sides[1] > sides[2])
+  && (sides[1] + sides[2] > sides[0])
+  && (sides[2] + sides[0] > sides[1]));
 }
 
 export {
