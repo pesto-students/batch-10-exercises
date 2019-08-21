7f586bc1c3ccad6aab3cba3d0adea9d22e02c41a
hacknaman <hacknaman@gmail.com>
2019-08-21 16:54:38 +0530

    done isodd

--- is-odd/isOdd.js
+++ is-odd/isOdd.js
@@ -1,6 +1,14 @@
+function myIsEven(val) {
+  if(val == 0)
+    return true;
+  else if(val < 0)
+    return false;
+
+  return myIsEven(val - 2);
+}
 
 function isOdd(...args) {
-  return args;
+  return !myIsEven(Math.abs(args[0]));
 }
 
 export {
