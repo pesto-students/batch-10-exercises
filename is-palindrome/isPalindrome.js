2cb75ec7a13e36e567e9fb5426657f9fc556fc83
hacknaman <hacknaman@gmail.com>
2019-08-21 19:59:32 +0530

    partially completed is Palindrome

--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,6 +1,21 @@
+function isPalindromeString(str) {
 
-function isPalindrome(...args) {
-  return args;
+  if(str.length <= 1){
+    return true;
+  }
+
+  var passedStr = str.slice(1, -1);
+
+  return str[0] == str[str.length-1] && isPalindromeString(passedStr);
+}
+
+function isPalindrome(numberOrString) {
+
+  if(typeof numberOrString != 'number' && typeof numberOrString != 'string'){
+    throw `Expected input should of type String or Number. You entered $(typeof numberOrString)`; 
+  }
+
+  return isPalindromeString(numberOrString.toString());
 }
 
 export {
