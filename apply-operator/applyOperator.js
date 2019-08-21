be782e19f5be7d00f7785f3d222a930025a83af1
thapasya-m <thapasya.murali@gmail.com>
2019-08-21 17:07:19 +0530

    test operator pass 1 - in prog

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,9 +1,26 @@
 
 function applyOperator(...args) {
-  if (args.length === 0) {
-    return 0;
+  let result = 0;
+  if (args.length === 1) {
+    return result;
   }
-  return args;
+  if (args[0] === '*' || args[0] === '/' || args[0] === '%') {
+    result = 1;
+  }
+  for (let i = 1; i < args.length; i += 1) {
+    if (args[0] === '+') {
+      result = args[i] + result;
+    } else if (args[0] === '-') {
+      result -= args[i];
+    } else if (args[0] === '*') {
+      result *= args[i];
+    } else if (args[0] === '/') {
+      result /= args[i];
+    } else if (args[0] === '%') {
+      result %= args[i];
+    }
+  }
+  return result;
 }
 
 export {
