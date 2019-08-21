4b20adf0316fff3a8eb325ee6f4d645f213503a3
hacknaman <hacknaman@gmail.com>
2019-08-21 20:11:34 +0530

    done reverse string

--- reverse-string/reverseString.js
+++ reverse-string/reverseString.js
@@ -1,6 +1,17 @@
 
-function reverseString(...args) {
-  return args;
+function reverseString(str) {
+
+  if(typeof str != 'string'){
+    throw "Expected input should be a string.";
+  }
+
+  var reversedStr = '';
+  while (str.length != 0){
+    reversedStr += str.charAt(str.length-1);
+    str = str.slice(0,-1);
+  }
+
+  return reversedStr;
 }
 
 export {
