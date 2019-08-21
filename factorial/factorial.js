9184e21f438879b3ea97f3043c5b5cfe1e43fa78
Ankeeth Suvarna <ankitsuvarna11@gmail.com>
2019-08-21 17:01:35 +0530

    completed factorial

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,8 +1,7 @@
-
-function factorial(...args) {
-  return args;
+function factorial(num) {
+  if (typeof num !== 'number') throw Error('Argument must be a number');
+  if (num === 0) return 1;
+  return num * factorial(num - 1);
 }
 
-export {
-  factorial,
-};
+export { factorial };
