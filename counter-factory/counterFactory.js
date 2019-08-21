73e5ebddd22cb30d61b4ef6e37d0ed3397ca350f
Ankeeth Suvarna <ankitsuvarna11@gmail.com>
2019-08-21 16:57:11 +0530

    completed counter-factory

--- counter-factory/counterFactory.js
+++ counter-factory/counterFactory.js
@@ -1,6 +1,17 @@
 
-function counterFactory(...args) {
-  return args;
+function counterFactory() {
+  let incrementCounter = 0;
+  let decrementCounter = 0;
+  return {
+    increment: () => {
+      incrementCounter += 1;
+      return incrementCounter;
+    },
+    decrement: () => {
+      decrementCounter -= 1;
+      return decrementCounter;
+    },
+  };
 }
 
 export {
