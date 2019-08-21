d18eed262498484434aab3564169633e0b0d5258
somaran <sarathsomana@gmail.com>
2019-08-21 19:43:03 +0530

    Solves is-palindrome

--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,6 +1,8 @@
 
-function isPalindrome(...args) {
-  return args;
+function isPalindrome(value) {
+  const ACCEPTED_STRING_REGEX = /[^a-zA-Z0-9]/g;
+  const string = String(value).replace(ACCEPTED_STRING_REGEX, '').toLowerCase();
+  return string === string.split('').reverse().join('');
 }
 
 export {
