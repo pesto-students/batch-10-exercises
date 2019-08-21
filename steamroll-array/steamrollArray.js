cd4b6283ec8c03651c70b949ed0836dc327a3939
somaran <sarathsomana@gmail.com>
2019-08-22 01:19:53 +0530

    Solves steamrollarray

--- steamroll-array/steamrollArray.js
+++ steamroll-array/steamrollArray.js
@@ -1,6 +1,10 @@
+/* eslint-disable arrow-body-style */
 
-function steamrollArray(...args) {
-  return args;
+function steamrollArray(array) {
+  const steamRollReducer = (flatArray, element) => {
+    return flatArray.concat(Array.isArray(element) ? steamrollArray(element) : element);
+  };
+  return array.reduce(steamRollReducer, []);
 }
 
 export {
