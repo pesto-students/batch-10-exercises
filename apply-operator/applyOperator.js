603fef5a41aa5b30f8cc2acf24cef734ae66717b
Ankeeth Suvarna <ankitsuvarna11@gmail.com>
2019-08-21 16:32:03 +0530

    completed apply-operator

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,8 +1,13 @@
+function applyOperator(operator, ...args) {
+  if (!operator) throw Error('Operator required');
+  if (args.length < 0) return 0;
+  if (operator === '%' && args.length === 1) return args[0];
 
-function applyOperator(...args) {
-  return args;
+  let defaultValue = 1;
+  if (operator === '+' || operator === '-') defaultValue = 0;
+  // eslint-disable-next-line no-eval
+  const result = args.reduce((acc, operand) => eval(`${acc}${operator}${operand}`), defaultValue);
+  return result;
 }
 
-export {
-  applyOperator,
-};
+export { applyOperator };
