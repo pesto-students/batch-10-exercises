dc8e6daff980b084b1d93c8568d2f4456aa3f34d
somaran <sarathsomana@gmail.com>
2019-08-23 00:12:55 +0530

    Solves alternating characters

--- alternating-characters/alternatingCharacters.js
+++ alternating-characters/alternatingCharacters.js
@@ -1,6 +1,16 @@
 
-function alternatingCharacters(...args) {
-  return args;
+function alternatingCharacters(strings) {
+  return strings.map((string) => {
+    const characters = string.split('');
+    let numberOfDeletions = 0;
+    characters.reduce((previousCharacter, currentCharacter) => {
+      if (previousCharacter === currentCharacter) {
+        numberOfDeletions += 1;
+      }
+      return currentCharacter;
+    });
+    return numberOfDeletions;
+  });
 }
 
 export {
