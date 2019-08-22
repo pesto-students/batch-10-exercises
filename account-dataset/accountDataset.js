6164645606eb6f4facd891e5c5f658b55061955e
thapasya-m <thapasya.murali@gmail.com>
2019-08-22 19:31:40 +0530

    bank balance test - passed

--- account-dataset/accountDataset.js
+++ account-dataset/accountDataset.js
@@ -1,8 +1,13 @@
+import dataset from './dataset.json';
 
 function accountDataset(...args) {
   return args;
 }
 
+function hundredThousandairs() {
+  return dataset.bankBalances.filter((data) => parseFloat(data.amount) > 100000);
+}
 export {
   accountDataset,
+  hundredThousandairs,
 };
