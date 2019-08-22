38ff3388586e644522ed15e28fd79cbfcff5aa08
ArpitKumar2092 <arpitkumar2092@gmail.com>
2019-08-22 20:00:49 +0530

    Updated squareNumber

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,11 +1,12 @@
 
-function squareNumbersArray(...numbers) {
+function squareNumbersArray(...inputs) {
+  let numbers = inputs.shift()
   let squares = numbers.map((number)=>{
     if(!parseInt(number)){
       throw new Error(`"${number}" must be number`);
     }
     return Math.pow(number , 2);
-  })
+  });
   return squares;
 }
 
