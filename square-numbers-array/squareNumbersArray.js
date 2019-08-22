a719cdc19704cca33b392b6d12e7334d56c4a1ae
somaran <sarathsomana@gmail.com>
2019-08-22 22:23:02 +0530

    Solves square-numbers-array

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,6 +1,11 @@
+/* eslint-disable no-restricted-globals */
 
-function squareNumbersArray(...args) {
-  return args;
+function squareNumbersArray(...array) {
+  const isNumber = (value) => typeof value === 'number';
+  if (!array.every(isNumber)) {
+    throw new Error('Expected numbers as array elements');
+  }
+  return array.map((element) => element ** 2);
 }
 
 export {
