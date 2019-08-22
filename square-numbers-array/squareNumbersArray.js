efb2696a03209f1dc3602e630cba5dd7eb8b8969
p-racoon <prasham9.ash@gmail.com>
2019-08-22 23:21:55 +0530

    completed squareNumbersArray

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,22 +1,36 @@
 
-function squareNumbersArray(mixedArray) {
-  function checkIfNumbersArray(mixedArray) {
-    mixedArray.forEach(element => {
-      if (typeof element != 'number')
+function squareNumbersArray(...unsanitizedArray) {
+  function checkIfNumbers(unsanitizedArray) {
+    for (const element of unsanitizedArray) {
+      if (isNaN(element)) {
         return typeof element;
-    });
+      }
+    }
     return true;
   }
-  const numbersArray = [];
-  const isNumbersArray=checkIfNumbersArray;
-  if (isNumbersArray===true) {
-    numbersArray = mixedArray;
+  let squaredNumbers = [];
+  const isNumbers = checkIfNumbers(unsanitizedArray);
+  if (isNumbers === true) {
+    let numbers = unsanitizedArray;
+    squaredNumbers = numbers.map(element => Math.pow(element, 2));
   }
   else {
-    throw Error(`Array of numbers expected. Found ${isNumbersArray}`)
+    throw new Error(`Array of numbers expected. Found ${isNumbers}`);
   }
-  return args;
+  return squaredNumbers;
 }
+
+// function squareNumbersArray(...unsanitizedArray) {
+//   let squaredNumbers = [];
+//   if (unsanitizedArray.every(element => !isNaN(element))) {
+//     let numbers = unsanitizedArray;
+//     squaredNumbers = numbers.map(element => element ** 2);
+//   }
+//   else {
+//     throw new Error(`Array of numbers is expected. `)
+//   }
+//   return squaredNumbers;
+// }
 
 export {
   squareNumbersArray,
