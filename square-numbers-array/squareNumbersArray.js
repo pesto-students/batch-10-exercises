115560294d2d6f17718b6d0e425249c50db4a0a4
hacknaman <hacknaman@gmail.com>
2019-08-22 19:34:12 +0530

    done squareNumbersArray

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,6 +1,15 @@
 
-function squareNumbersArray(...args) {
-  return args;
+function squareNumbersArray(...maybeNumbers) {
+  maybeNumbers.forEach(element => {
+    if(typeof element !== 'number' ) {
+      throw Error("My custom error");
+    }
+  });
+ 
+  var numbers = maybeNumbers.map( (x)=>{ 
+    return Math.pow(x,2) 
+  });
+  return numbers;
 }
 
 export {
