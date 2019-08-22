cd135e84287263c1b63476ed765eef1ee072422e
p-racoon <prasham9.ash@gmail.com>
2019-08-22 15:11:57 +0530

    diff-array

--- diff-array/diffArray.js
+++ diff-array/diffArray.js
@@ -1,6 +1,31 @@
-
-function diffArray(...args) {
-  return args;
+function diffArray(firstArray, secondArray) {
+  function checkIfElementExists(elementOfFirstArray, secondArray) {
+    if (secondArray.indexOf(elementOfFirstArray) >= 0) {
+      return true;
+    }
+    return false;
+  }
+  function getMergedArray(array1) {
+    return (array2) => [...array1, ...array2]
+  }
+  function removeFromBothCopy(elementToBeRemoved) {
+    firstArrayCopy = firstArrayCopy.filter(element => {
+      return element != elementToBeRemoved;
+    })
+    secondArrayCopy = secondArrayCopy.filter(element => {
+      return element != elementToBeRemoved;
+    })
+  }
+  let firstArrayCopy = [...firstArray];
+  let secondArrayCopy = [...secondArray];
+  let symmetricDifference = [];
+  for (const element of firstArray) {
+    if (checkIfElementExists(element, secondArray)) {
+      removeFromBothCopy(element)
+    }
+  }
+  symmetricDifference = getMergedArray(firstArrayCopy)(secondArrayCopy)
+  return symmetricDifference;
 }
 
 export {
