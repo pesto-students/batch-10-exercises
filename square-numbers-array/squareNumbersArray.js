b6cef5f9d14a28b897229d9b4ea4993f66a108c6
p-racoon <prasham9.ash@gmail.com>
2019-08-22 19:47:31 +0530

    squareNumbersArray in progress

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,5 +1,20 @@
 
-function squareNumbersArray(...args) {
+function squareNumbersArray(mixedArray) {
+  function checkIfNumbersArray(mixedArray) {
+    mixedArray.forEach(element => {
+      if (typeof element != 'number')
+        return typeof element;
+    });
+    return true;
+  }
+  const numbersArray = [];
+  const isNumbersArray=checkIfNumbersArray;
+  if (isNumbersArray===true) {
+    numbersArray = mixedArray;
+  }
+  else {
+    throw Error(`Array of numbers expected. Found ${isNumbersArray}`)
+  }
   return args;
 }
 
--- square-numbers-array/squareNumbersArray.test.js
+++ square-numbers-array/squareNumbersArray.test.js
@@ -3,7 +3,7 @@ import { squareNumbersArray } from './squareNumbersArray';
 describe('squareNumbersArray', () => {
   test('should throw an error if array of numbers is not passed as argument', () => {
     const myArray = [1, 2, 'string', 4];
-    expect(() => squareNumbersArray(...myArray)).toThrow('My custom error');
+    expect(() => squareNumbersArray(...myArray)).toThrow('Array of numbers expected. Found string');
   });
   test('should return square array', () => {
     expect(squareNumbersArray(1, 2, 3)).toEqual([1, 4, 9]);
