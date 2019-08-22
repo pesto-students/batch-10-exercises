a947c0a4d9c080d2a382121a4296a593aa6483c3
ArpitKumar2092 <arpitkumar2092@gmail.com>
2019-08-22 19:39:16 +0530

    Fixed SquareNumbers

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,6 +1,12 @@
 
-function squareNumbersArray(...args) {
-  return args;
+function squareNumbersArray(...numbers) {
+  let squares = numbers.map((number)=>{
+    if(!parseInt(number)){
+      throw new Error(`"${number}" must be number`);
+    }
+    return Math.pow(number , 2);
+  })
+  return squares;
 }
 
 export {
