2059e77923d89c14b211bbe9445049a22bf4250b
p-racoon <prasham9.ash@gmail.com>
2019-08-22 11:20:35 +0530

    completed binary-agent

--- binary-agent/binaryAgent.js
+++ binary-agent/binaryAgent.js
@@ -1,6 +1,15 @@
 
-function binaryAgent(...args) {
-  return args;
+function binaryAgent(binaryString) {
+  function getASCIICharacter(eachBinaryCharacter) {
+    const equivalentDecimal = parseInt(eachBinaryCharacter, 2)
+    const ASCIICharacter = String.fromCharCode(equivalentDecimal)
+    return ASCIICharacter
+  }
+  const binaryCharactersArray = String(binaryString).split(' ');
+  let result = '';
+  binaryCharactersArray.map(eachBinaryCharacter =>
+    result += getASCIICharacter(eachBinaryCharacter));
+  return result;
 }
 
 export {
