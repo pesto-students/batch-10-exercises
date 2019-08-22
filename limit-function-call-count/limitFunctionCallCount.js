75ce8f4d574189707093bc794629ab7d7af52ea9
hacknaman <hacknaman@gmail.com>
2019-08-22 11:27:43 +0530

    submitted limit function test

--- limit-function-call-count/limitFunctionCallCount.js
+++ limit-function-call-count/limitFunctionCallCount.js
@@ -1,6 +1,19 @@
 
-function limitFunctionCallCount(...args) {
-  return args;
+function limitFunctionCallCount(func, maxCount) {
+
+  var _func = func;
+  var _maxCount = maxCount;
+  var _currentCount = 0;
+ 
+  return (...args) => { 
+    if(_currentCount < _maxCount){
+      _currentCount++;
+      return _func(...args);
+    } 
+    else {
+      return null;
+    } 
+  }
 }
 
 export {
