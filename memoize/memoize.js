1e8ac3502e1bb28d325797aba6eed7cda22f7b1f
hacknaman <hacknaman@gmail.com>
2019-08-22 19:21:36 +0530

    done memoize (could be wrong)

--- memoize/memoize.js
+++ memoize/memoize.js
@@ -1,6 +1,14 @@
 
-function memoize(...args) {
-  return args;
+function memoize(func) {
+  var _func = func;
+  var _resultMap = {};
+
+  return (...args) => {
+    if(!_resultMap.hasOwnProperty(args)){
+      _resultMap[args] = _func(...args);   
+    }
+    return _resultMap[args];
+  }
 }
 
 export {
