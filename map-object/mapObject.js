016c0845b80546561b2ed9ef4ff14015d5405773
somaran <sarathsomana@gmail.com>
2019-08-23 11:55:48 +0530

    Solves map object

--- map-object/mapObject.js
+++ map-object/mapObject.js
@@ -1,6 +1,12 @@
 
-function mapObject(...args) {
-  return args;
+function mapObject(object, mapFunction) {
+  const keys = Object.keys(object);
+  const mappedObject = {};
+  for (let index = 0; index < keys.length; index += 1) {
+    const key = keys[index];
+    mappedObject[key] = mapFunction(object[key]);
+  }
+  return mappedObject;
 }
 
 export {
