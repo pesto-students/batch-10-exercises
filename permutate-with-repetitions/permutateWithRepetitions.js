a91e0e42de9096126f6076996140bcb952866a71
hacknaman <hacknaman@gmail.com>
2019-08-23 17:17:04 +0530

    place holder for permutateWith repetition

--- permutate-with-repetitions/permutateWithRepetitions.js
+++ permutate-with-repetitions/permutateWithRepetitions.js
@@ -1,6 +1,21 @@
+function permutateWithRepetitions(lettersArray) {
+  var totalPermutation = Math.pow ( lettersArray.length, lettersArray.length);
 
-function permutateWithRepetitions(...args) {
-  return args;
+  var allPermutation = [];
+
+  // to arrays
+  for(let i = 0; i < totalPermutation; ++i) {
+    var numberWithBase = i.toString(lettersArray.length);
+    const numberWithBaseLen = numberWithBase.length;
+    for(let j = 0; j < lettersArray.length - numberWithBaseLen; j++ ) {
+      numberWithBase = "0" + numberWithBase;
+    }
+    console.log(numberWithBase);
+    allPermutation.push(numberWithBase);
+  }
+
+  // map array to letters array
+  return allPermutation;
 }
 
 export {
