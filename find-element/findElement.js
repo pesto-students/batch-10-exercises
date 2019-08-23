e53eaa6362c634b65c5c240226bfd8581a86813d
somaran <sarathsomana@gmail.com>
2019-08-23 11:38:37 +0530

    Solves find element

--- find-element/findElement.js
+++ find-element/findElement.js
@@ -1,6 +1,10 @@
 
-function findElement(...args) {
-  return args;
+function findElement(array, testCondition) {
+  for (let index = 0; index < array.length; index += 1) {
+    const value = array[index];
+    if (testCondition(value)) return value;
+  }
+  return undefined;
 }
 
 export {
