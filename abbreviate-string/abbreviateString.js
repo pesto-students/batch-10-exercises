4f21bdd0367f080735e809d0ca629ea76c6edc07
hacknaman <hacknaman@gmail.com>
2019-08-23 16:43:22 +0530

    beautiful abbreviateString function that works

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,6 +1,15 @@
 
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(possibleString) {
+  if( typeof possibleString != 'string' ) {
+    throw new Error("throws error on invalid parameters");
+  }
+
+  const stringArray = possibleString.split(' ');
+  const firstName = stringArray[0];
+  const lastName = stringArray[stringArray.length - 1];
+  const uppercaseLastLetterWithADot = lastName[0].toUpperCase() + '.';
+
+  return firstName + " " + uppercaseLastLetterWithADot;
 }
 
 export {
