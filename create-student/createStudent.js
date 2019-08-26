1f5de5adbb9874a604eac1843cdfa08b782a0d06
Ankeeth Suvarna <ankitsuvarna11@gmail.com>
2019-08-26 16:41:19 +0530

    completed create-student

--- create-student/createStudent.js
+++ create-student/createStudent.js
@@ -1,8 +1,11 @@
-
-function createStudent(...args) {
-  return args;
+function createStudent({ likesJavaScript = true, likesES2015 = true } = {}) {
+  if (!likesJavaScript && !likesES2015) {
+    return 'The student does not like much...';
+  }
+  if (likesJavaScript && likesES2015) {
+    return 'The student likes JavaScript and ES2015';
+  }
+  return `The student likes ${likesJavaScript ? 'JavaScript' : 'ES2015'}!`;
 }
 
-export {
-  createStudent,
-};
+export { createStudent };
