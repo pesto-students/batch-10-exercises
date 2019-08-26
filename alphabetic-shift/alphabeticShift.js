15eec094be4ede47447e29ed79aa7743e71477a1
Sarath Kumar Somana <sarathsomana@gmail.com>
2019-08-26 23:45:38 +0530

    Solves alphabetic shift

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,6 +1,18 @@
+/* eslint-disable arrow-body-style */
 
-function alphabeticShift(...args) {
-  return args;
+function alphabeticShift(string) {
+  const upperCaseAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZA';
+  const lowerCaseAlphabet = 'abcdefghijklmnopqrstuvwxyza';
+  const isLower = (character) => lowerCaseAlphabet.includes(character);
+  const nextCharacter = (alphabet) => (character) => alphabet[alphabet.indexOf(character) + 1];
+  const nextLowerCharacter = nextCharacter(lowerCaseAlphabet);
+  const nextUpperCharacter = nextCharacter(upperCaseAlphabet);
+
+  const characters = string.split('');
+  const shiftedCharacters = characters.map((character) => {
+    return isLower(character) ? nextLowerCharacter(character) : nextUpperCharacter(character);
+  });
+  return shiftedCharacters.join('');
 }
 
 export {
