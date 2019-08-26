5ffbe645f59717ce5710795cb4121c790bbdbad7
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-08-26 23:15:51 +0530

    greet

--- greet/greet.test.js
+++ greet/greet.test.js
@@ -4,5 +4,6 @@ describe('greet', () => {
   test('The function should return a Promise', () => {
     expect(greet() instanceof Promise).toBe(true);
   });
-  test('Promise resolve value', () => expect(greet('Pesto')).resolves.toBe('Hey Pesto'));
+  test('Promise resolve value', () =>
+    expect(greet('Pesto')).resolves.toBe('Hey Pesto'));
 });
