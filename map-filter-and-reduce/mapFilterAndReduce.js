f0d4e02ef9d0a10c6eca005c0ac581e8c32e12ca
Sarath Kumar Somana <sarathsomana@gmail.com>
2019-08-26 16:43:58 +0530

    Solves map-filter-and-reduce

--- map-filter-and-reduce/mapFilterAndReduce.js
+++ map-filter-and-reduce/mapFilterAndReduce.js
@@ -1,6 +1,12 @@
 
-function mapFilterAndReduce(...args) {
-  return args;
+function mapFilterAndReduce(arrayOfObjects) {
+  const firstNamesArray = arrayOfObjects.map((object) => object.firstName);
+  const arrayWithShortNames = firstNamesArray.filter((name) => name.length < 5);
+  const namesAndLengthsObject = arrayWithShortNames.reduce((object, name) => ({
+    ...object,
+    [name]: name.length,
+  }), {});
+  return namesAndLengthsObject;
 }
 
 export {
