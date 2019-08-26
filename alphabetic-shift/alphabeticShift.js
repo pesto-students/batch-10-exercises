f4178a086de5ed35e6d9673e12930a11034bd7c5
Ankeeth Suvarna <ankitsuvarna11@gmail.com>
2019-08-26 23:07:27 +0530

    completed alphabetic-shift

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,6 +1,14 @@
 
-function alphabeticShift(...args) {
-  return args;
+function alphabeticShift(str) {
+  const getCharCode = (c) => c.charCodeAt(0);
+  const shiftCodeBy1Place = (code) => code + 1;
+  const split = (splitter) => (string) => string.split(splitter);
+
+  const noSpaceSplitter = split('');
+  const splitString = noSpaceSplitter(str);
+
+  const shiftedCharacters = splitString.map(getCharCode).map(shiftCodeBy1Place);
+  return String.fromCharCode(...shiftedCharacters);
 }
 
 export {
