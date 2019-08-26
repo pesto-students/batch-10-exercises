050a2e11d26ddc95494e6c6931ed38d3a47a847d
thapasya-m <thapasya.murali@gmail.com>
2019-08-26 15:43:05 +0530

    alphabt test

--- add-big-integers/addBigIntegers.js
+++ add-big-integers/addBigIntegers.js
@@ -1,6 +1,10 @@
 
-function addBigIntegers(...args) {
-  return args;
+function addBigIntegers(args) {
+  const inputString = args.split('\n');
+  inputString.shift();
+  const sumOfNum = (sum, value) => parseInt(sum, 10) + parseInt(value, 10);
+  const sum = [...inputString].reduce(sumOfNum);
+  return sum.toString();
 }
 
 export {
