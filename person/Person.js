c7c86608b7507ca05fd912e06c5ec314c41ba4b8
Sarath Kumar Somana <sarathsomana@gmail.com>
2019-08-27 00:02:31 +0530

    Solves person

--- person/Person.js
+++ person/Person.js
@@ -1,8 +1,25 @@
+/* eslint-disable no-restricted-globals */
 
-function person(...args) {
-  return args;
+class Person {
+  constructor(firstName, lastName, dateOfBirth) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  addDobDigits() {
+    const toNumber = (character) => parseInt(character, 10);
+    const isNumber = (character) => !isNaN(toNumber(character));
+    const sum = (a, b) => a + b;
+
+    const digitsInDob = this.dateOfBirth
+      .split('')
+      .filter(isNumber)
+      .map(toNumber);
+    return digitsInDob.reduce(sum, 0);
+  }
 }
 
 export {
-  person,
+  Person,
 };
