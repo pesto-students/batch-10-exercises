ac186e3e62174b11898d57448e4c670089fa595d
Sarath Kumar Somana <sarathsomana@gmail.com>
2019-08-26 19:53:22 +0530

    Solves is promise

--- is-promise/isPromise.js
+++ is-promise/isPromise.js
@@ -1,6 +1,11 @@
+/* eslint-disable valid-typeof */
 
-function isPromise(...args) {
-  return args;
+function isPromise(mayBePromise) {
+  const isOfType = (type) => (object) => typeof object === type;
+  const isObject = isOfType('object');
+  const isFunction = isOfType('function');
+  const isThenable = (object) => !!object && isFunction(object.then);
+  return (isObject(mayBePromise) || isFunction(mayBePromise)) && isThenable(mayBePromise);
 }
 
 export {
