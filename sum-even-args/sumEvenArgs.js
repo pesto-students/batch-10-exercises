7c5de9045dcfc6ff928f03931ad7c7a01b427c71
Sarath Kumar Somana <sarathsomana@gmail.com>
2019-08-26 17:05:41 +0530

    Solves even sum args

--- sum-even-args/sumEvenArgs.js
+++ sum-even-args/sumEvenArgs.js
@@ -1,6 +1,9 @@
 
 function sumEvenArgs(...args) {
-  return args;
+  const isEven = (number) => number % 2 === 0;
+  const evenArgs = args.filter(isEven);
+  const sum = (a, b) => a + b;
+  return evenArgs.reduce(sum, 0);
 }
 
 export {
