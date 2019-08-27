c2f3872c765330da44d7ccd6b672394f1804ff93
Sarath Kumar Somana <sarathsomana@gmail.com>
2019-08-27 19:45:01 +0530

    Solves ackermann

--- ackermann/ackermann.js
+++ ackermann/ackermann.js
@@ -1,6 +1,14 @@
 
-function ackermann(...args) {
-  return args;
+function ackermann(inputs) {
+  const isZero = (number) => number === 0;
+  const { m, n } = inputs;
+  if (isZero(m)) {
+    return n + 1;
+  }
+  if (isZero(n)) {
+    return ackermann({ m: m - 1, n: 1 });
+  }
+  return ackermann({ m: m - 1, n: ackermann({ m, n: n - 1 }) });
 }
 
 export {
