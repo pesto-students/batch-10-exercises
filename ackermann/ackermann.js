15fe0de8480d8429bc34657411e72927f3cc27fd
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-08-27 19:45:46 +0530

    Ackermann

--- ackermann/ackermann.js
+++ ackermann/ackermann.js
@@ -1,8 +1,16 @@
+const ackermann = args => {
+  const { m, n } = args;
+  if (m === 0) {
+    return n + 1;
+  }
 
-function ackermann(...args) {
-  return args;
-}
+  if (n === 0) {
+    return ackermann({ m: m - 1, n: 1 });
+  }
 
-export {
-  ackermann,
+  if (m !== 0 && n !== 0) {
+    return ackermann({ m: m - 1, n: ackermann({ m, n: n - 1 }) });
+  }
 };
+
+export { ackermann };
