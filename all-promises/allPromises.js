825072421c6ca13cf5c3b41babbd651513982dc8
p-racoon <prasham9.ash@gmail.com>
2019-08-27 10:27:49 +0530

    completed all-promises

--- all-promises/allPromises.js
+++ all-promises/allPromises.js
@@ -1,8 +1,17 @@
-
-function allPromises(...args) {
-  return args;
+function allPromises(promises) {
+  return new Promise((resolve, reject) => {
+    const promiseValues = [];
+    promises.forEach((promise, index) => {
+      if (promise instanceof Promise) {
+        promise.then(value => {
+          promiseValues[index] = value;
+        });
+      } else {
+        const garbageValue = promise;
+        promiseValues[index] = garbageValue;
+      }
+    });
+    resolve(promiseValues);
+  });
 }
-
-export {
-  allPromises,
-};
+export { allPromises };
