ee1c500b66da618c8869dccd1f5708138f053133
p-racoon <prasham9.ash@gmail.com>
2019-08-27 16:41:33 +0530

    completed isIterable

--- is-iterable/isIterable.js
+++ is-iterable/isIterable.js
@@ -1,8 +1,5 @@
-
-function isIterable(...args) {
-  return args;
+function isIterable(maybeIterable) {
+  return typeof maybeIterable[Symbol.iterator] === 'function';
 }
 
-export {
-  isIterable,
-};
+export { isIterable };
