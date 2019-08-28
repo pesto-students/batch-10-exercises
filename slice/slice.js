b418d7f46b8eccfa6b3e70532094f2073982619a
thapasya-m <thapasya.murali@gmail.com>
2019-08-28 21:04:39 +0530

    slice

--- slice/slice.js
+++ slice/slice.js
@@ -1,6 +1,15 @@
 
-function slice(...args) {
-  return args;
+function slice(startIndex, endIndex, inputArray) {
+  const arrayLen = inputArray.length;
+  const endIndexInRange = endIndex >= arrayLen ? endIndex : (arrayLen + endIndex) % arrayLen;
+  const startIndexInRange = startIndex >= 0 ? startIndex : (arrayLen + startIndex) % arrayLen;
+  const outputArray = [...inputArray]
+    // .slice(startIndex, endIndex)
+    .filter((value, index) => index < endIndexInRange && index >= startIndexInRange);
+  if (typeof inputArray === 'string') {
+    return outputArray.join('');
+  }
+  return outputArray;
 }
 
 export {
