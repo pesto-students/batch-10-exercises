e3b478fbab8294f875858d989a5e8859fe247424
thapasya-m <thapasya.murali@gmail.com>
2019-08-28 17:00:44 +0530

    onchange partial pass

--- on-change/onChange.js
+++ on-change/onChange.js
@@ -1,6 +1,24 @@
 
-function onChange(...args) {
-  return args;
+function onChange(input, inputFn) {
+  const p = new Proxy(input, {
+    get(target, propKey) {
+      inputFn();
+      return target[propKey];
+    },
+    defineProperty(target, propKey, value) {
+      inputFn();
+      return Reflect.defineProperty(target, propKey, value);
+    },
+    set(target, propKey, value) {
+      inputFn();
+      return Reflect.set(target, propKey, value);
+    },
+    deleteProperty(target, propKey) {
+      inputFn();
+      return Reflect.deleteProperty(target, propKey);
+    },
+  });
+  return p;
 }
 
 export {
