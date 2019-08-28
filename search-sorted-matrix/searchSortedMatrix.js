33a5abe35166b93c53e65ccd537b72852176be3d
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-08-28 20:06:06 +0530

    searchSortedMatrix

--- search-sorted-matrix/searchSortedMatrix.js
+++ search-sorted-matrix/searchSortedMatrix.js
@@ -1,8 +1,9 @@
-
-function searchSortedMatrix(...args) {
-  return args;
+function searchSortedMatrix(obj) {
+  const { search, matrix } = obj;
+  const potentialArr = matrix.filter(arr => {
+    return [arr[0] <= search && arr[arr.length - 1] >= search][0];
+  });
+  return potentialArr[0].includes(search);
 }
 
-export {
-  searchSortedMatrix,
-};
+export { searchSortedMatrix };
