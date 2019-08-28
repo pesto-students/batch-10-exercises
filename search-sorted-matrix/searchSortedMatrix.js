aea375b5a224a0354fa57255dfec6583cb7071f3
thapasya-m <thapasya.murali@gmail.com>
2019-08-28 19:43:19 +0530

    [WIP] Complete day 8 segment 2

--- search-sorted-matrix/searchSortedMatrix.js
+++ search-sorted-matrix/searchSortedMatrix.js
@@ -1,6 +1,9 @@
 
-function searchSortedMatrix(...args) {
-  return args;
+function searchSortedMatrix(input) {
+  const { search, matrix } = input;
+  return matrix
+    .filter((el) => el.indexOf(search) > -1)
+    .length > 0;
 }
 
 export {
