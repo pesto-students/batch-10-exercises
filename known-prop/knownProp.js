c92bf9ebe520280a5ac83c044ba9efa6f232aed3
ArpitKumar2092 <arpitkumar2092@gmail.com>
2019-08-28 16:38:48 +0530

    Updated KnownProp

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -1,7 +1,17 @@
 
-function knownProp(...args) {
-  return args;
+function knownProp( collection ) {
+  return new Proxy( collection , Handler );
 }
+const Handler = {
+  get: function(target , key, value) {
+    for(let keyInTarget in target){
+      if(keyInTarget === key){
+        return target[key];
+      }
+    }
+    throw new TypeError('Unknown property');
+  }
+} 
 
 export {
   knownProp,
