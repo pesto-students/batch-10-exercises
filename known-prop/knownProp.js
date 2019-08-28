d7f0f819549ca5396c7614b3b505513d23e69d67
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-08-28 16:12:16 +0530

    knownProp

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -1,8 +1,13 @@
-
-function knownProp(...args) {
-  return args;
+function knownProp(obj) {
+  const objProxy = new Proxy(obj, {
+    get(target, propKey) {
+      if (propKey in target) {
+        return target[propKey];
+      }
+      throw new TypeError('Unknown property');
+    }
+  });
+  return objProxy;
 }
 
-export {
-  knownProp,
-};
+export { knownProp };
