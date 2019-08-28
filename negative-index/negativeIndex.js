ec6938f0628a922d5a45c288b936d0f971e93e72
thapasya-m <thapasya.murali@gmail.com>
2019-08-28 16:40:45 +0530

    neg-idex

--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,6 +1,31 @@
+/* eslint-disable no-restricted-globals */
 
-function negativeIndex(...args) {
-  return args;
+function negativeIndex(input) {
+  if (!(input instanceof Array)) {
+    throw new TypeError('Only arrays are supported');
+  }
+  const p = new Proxy(input, {
+    get(target, propKey) {
+      if (typeof propKey === 'string') {
+        const maybeIndex = parseInt(propKey, 10);
+        if (!isNaN(maybeIndex)) {
+          const len = target.length;
+          return target[(len + maybeIndex) % len];
+        }
+      }
+      return target[propKey];
+    },
+    set(target, propKey, value) {
+      const maybeIndex = parseInt(propKey, 10);
+      if (!isNaN(maybeIndex)) {
+        const len = target.length;
+        const prop = (len + maybeIndex) % len;
+        return Reflect.set(target, prop, value);
+      }
+      return true;
+    },
+  });
+  return p;
 }
 
 export {
