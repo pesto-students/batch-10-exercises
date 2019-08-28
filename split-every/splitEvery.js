cc8562dc07aa776f1e617df9d3dc75102f3fa35f
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-08-29 03:28:55 +0530

    splitEvery

--- split-every/splitEvery.js
+++ split-every/splitEvery.js
@@ -1,8 +1,15 @@
-
 function splitEvery(...args) {
-  return args;
+  const [size, inputArray] = [...args];
+  if (size <= 0) {
+    throw new Error(
+      `Received invalid input ${size}. Please enter a positive integer.`
+    );
+  }
+  function* chunks(arr, n) {
+    for (let i = 0; i < arr.length; i += n) {
+      yield arr.slice(i, i + n);
+    }
+  }
+  return [...chunks(inputArray, size)];
 }
-
-export {
-  splitEvery,
-};
+export { splitEvery };
