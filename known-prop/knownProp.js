029b737f936ae84fba82ccea6ca5cb76e76ab0ca
Ankeeth Suvarna <ankitsuvarna11@gmail.com>
2019-08-28 16:39:51 +0530

    completed known-prop

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -1,8 +1,15 @@
-
-function knownProp(...args) {
-  return args;
+function knownProp(obj) {
+  const proxy = new Proxy(obj, {
+    get(target, property) {
+      // eslint-disable-next-line no-prototype-builtins
+      const isInvalidProperty = !target.hasOwnProperty(property);
+      if (isInvalidProperty) {
+        throw new TypeError(`Unknown property ${property}`);
+      }
+      return Reflect.get(target, property);
+    },
+  });
+  return proxy;
 }
 
-export {
-  knownProp,
-};
+export { knownProp };
