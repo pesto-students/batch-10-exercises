ce20aa8dd1a24fc897fb1e6337c348924e6f40b5
p-racoon <prasham9.ash@gmail.com>
2019-08-29 17:05:40 +0530

    incomplete binarySearch

--- binary-search/binarySearch.js
+++ binary-search/binarySearch.js
@@ -1,8 +1,28 @@
-
-function binarySearch(...args) {
-  return args;
+function binarySearch(inputArr, searchTerm, customComparator) {
+  const comparator = customComparator
+    ? customComparator
+    : (a, b) => {
+        if (a === b) return 0;
+        return a < b ? -1 : 1;
+      };
+  const bSearch = searchTerm => sortedArr => {
+    if (sortedArr.length === 0) {
+      console.log("emptyArr :", sortedArr.length);
+      return -1;
+    }
+    // console.log("searchTerm, sortedArr :", searchTerm, sortedArr);
+    const midPointerPosition = Math.floor(sortedArr.length / 2);
+    const valueAtMid = sortedArr[midPointerPosition];
+    return comparator(searchTerm, valueAtMid) === 0
+      ? midPointerPosition
+      : comparator(searchTerm, valueAtMid) === 1
+      ? bSearch(searchTerm)(sortedArr.slice(midPointerPosition))
+      : comparator(searchTerm, valueAtMid) === -1
+      ? bSearch(searchTerm)(sortedArr.slice(0, midPointerPosition))
+      : null;
+  };
+  const searchTermExists = bSearch(searchTerm)(inputArr);
+  return searchTermExists;
 }
 
-export {
-  binarySearch,
-};
+export { binarySearch };
