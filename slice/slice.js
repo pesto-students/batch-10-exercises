cf8063029c4207ab3db95910decfb7a505486627
thapasya-m <thapasya.murali@gmail.com>
2019-08-29 12:54:21 +0530

    slice test

--- slice/slice.js
+++ slice/slice.js
@@ -4,7 +4,6 @@ function slice(startIndex, endIndex, inputArray) {
   const endIndexInRange = endIndex >= arrayLen ? endIndex : (arrayLen + endIndex) % arrayLen;
   const startIndexInRange = startIndex >= 0 ? startIndex : (arrayLen + startIndex) % arrayLen;
   const outputArray = [...inputArray]
-    // .slice(startIndex, endIndex)
     .filter((value, index) => index < endIndexInRange && index >= startIndexInRange);
   if (typeof inputArray === 'string') {
     return outputArray.join('');
