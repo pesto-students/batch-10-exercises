7023ee5d2cbd6f66b1f1984d862bebde14f32a30
Ankeeth Suvarna <ankitsuvarna11@gmail.com>
2019-08-30 17:12:07 +0530

    incomplete positioning practical

--- positioning-practical/positioningPractical.js
+++ positioning-practical/positioningPractical.js
@@ -1,8 +1,12 @@
-
-function positioningPractical(...args) {
-  return args;
+const tabs = document.getElementsByClassName('tab');
+const activeTab = 0;
+function toggleTabs(e) {
+  console.log(e);
 }
 
-export {
-  positioningPractical,
-};
+Array.prototype.forEach.call(tabs, (tab, index) => {
+  if (activeTab === index) {
+    tab.classList.add('active');
+  }
+  tab.addEventListener('click', toggleTabs);
+});
