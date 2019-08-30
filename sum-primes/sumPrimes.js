2b2c99691f76f81bc951d94fddbf1a69993897ae
Sarath Kumar Somana <sarathsomana@gmail.com>
2019-08-31 03:09:45 +0530

    Solves sum primes

--- sum-primes/sumPrimes.js
+++ sum-primes/sumPrimes.js
@@ -1,6 +1,18 @@
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+  const numbersArray = [...Array(number).keys()].slice(2);
+  const numbersTillSqrt = numbersArray.filter((n) => n <= Math.sqrt(number));
+  const factors = numbersTillSqrt.filter((n) => number % n === 0);
+  return factors.length === 0;
+};
 
-function sumPrimes(...args) {
-  return args;
+function sumPrimes(limit) {
+  const sum = (a, b) => a + b;
+
+  const numbers = [...Array(limit + 1).keys()].slice(2);
+  return numbers.filter(isPrime).reduce(sum);
 }
 
 export {
