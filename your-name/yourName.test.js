f6f8a203e9510ef8886bc0d5e0af4071cc1c7c9a
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-08-31 18:16:48 +0530

    CompletedAllTasks

--- your-name/yourName.test.js
+++ your-name/yourName.test.js
@@ -1,6 +1,4 @@
-import {
-  yourName
-} from './yourName';
+import { yourName } from './yourName';
 
 describe('Write your name in the test', () => {
   test('yourName should return my name', () => {
