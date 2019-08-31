1ab1f63924d288b13d8818b85aa801c895d3769b
thapasya-m <thapasya.murali@gmail.com>
2019-08-31 19:59:08 +0530

    memoization

--- cache-function/cacheFunction.js
+++ cache-function/cacheFunction.js
@@ -1,6 +1,15 @@
 
-function cacheFunction(...args) {
-  return args;
+function cacheFunction(inputFn) {
+  const cache = {};
+  return (...args) => {
+    const n = args[0];
+    if (n in cache) {
+      return cache[n];
+    }
+    const result = inputFn(n);
+    cache[n] = result;
+    return result;
+  };
 }
 
 export {
