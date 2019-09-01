53108f8a19e31f1962bec5b87b24b7faae1f95c8
p-racoon <prasham9.ash@gmail.com>
2019-09-01 22:29:55 +0530

    completed returnAStringtest

--- return-a-string-test/returnAStringTest.test.js
+++ return-a-string-test/returnAStringTest.test.js
@@ -2,6 +2,6 @@ const returnAString = () => 'helloworld';
 
 describe('returnAString', () => {
   test('should return a string', () => {
-    expect(returnAString()).toContain('world');
+    expect(typeof returnAString()).toBe('string');
   });
 });
