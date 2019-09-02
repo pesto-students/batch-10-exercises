04aeb9eb6c065350d4621422d75cddc04a38d555
p-racoon <prasham9.ash@gmail.com>
2019-09-02 12:10:47 +0530

    completed sumPrimes

--- sum-primes/sumPrimes.js
+++ sum-primes/sumPrimes.js
@@ -1,8 +1,38 @@
-
-function sumPrimes(...args) {
-  return args;
+function sumPrimes(limit) {
+  function isPrime(maybePrime) {
+    if (maybePrime === 0 || maybePrime === 1) {
+      return false;
+    }
+    for (let i = 2; i < maybePrime; i += 1) {
+      if (maybePrime % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  function* primeNumberGenerator() {
+    let i = 0;
+    while (true) {
+      if (isPrime(i)) {
+        yield i;
+      }
+      i += 1;
+    }
+  }
+  function getPrimes(upToLimit) {
+    const png = primeNumberGenerator();
+    const primeNumbers = [];
+    while (true) {
+      const primeNo = png.next().value;
+      if (primeNo > upToLimit) {
+        break;
+      } else {
+        primeNumbers.push(primeNo);
+      }
+    }
+    return primeNumbers;
+  }
+  return getPrimes(limit).reduce((sum, el) => sum + el);
 }
 
-export {
-  sumPrimes,
-};
+export { sumPrimes };
