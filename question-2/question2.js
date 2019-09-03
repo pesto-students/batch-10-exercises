12de192a7a465f88c6d465dc58afc55d6aaff9ec
p-racoon <prasham9.ash@gmail.com>
2019-09-03 18:28:25 +0530

    completed Q2 get values from a form

--- question-2/question2.js
+++ question-2/question2.js
@@ -1,8 +1,12 @@
-
-function question2(...args) {
-  return args;
+function getFormValue() {
+  event.preventDefault();
+  let firstName = document
+    .getElementById("nameForm")
+    .elements.namedItem("firstName").value;
+  let lastName = document
+    .getElementById("nameForm")
+    .elements.namedItem("lastName").value;
+  document.getElementsByClassName(
+    "fullName"
+  )[0].innerHTML = `Full name is: ${firstName} ${lastName}`;
 }
-
-export {
-  question2,
-};
