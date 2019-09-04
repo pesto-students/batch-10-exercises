1c152ffacfa7e9c4562b8f24ee7ec2b0294f91ac
thapasya-m <thapasya.murali@gmail.com>
2019-09-04 16:57:49 +0530

    hello world - text render test 1 of 2nd

--- beginner-exercises/src/components/01-HelloWorld.js
+++ beginner-exercises/src/components/01-HelloWorld.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 // Task: Render "Hello World" text.
+// eslint-disable-next-line react/prefer-stateless-function
 class HelloWorld extends Component {
   // All components *must* have a `render` method defined.
 
@@ -9,8 +10,10 @@ class HelloWorld extends Component {
   // JavaScript code. It is here just for readability purposes.
 
   render() {
+    const text = 'hello world!';
     return (
-      <h1>Fill this h1 tag with your heading!</h1>
+      // eslint-disable-next-line react/jsx-filename-extension
+      <h1>{text}</h1>
     );
   }
 }
