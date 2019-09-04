3135a93f0e4e30d18cc8bcaa69154c854cc16d3d
ArpitKumar2092 <arpitkumar2092@gmail.com>
2019-09-04 19:27:23 +0530

    uodating

--- duplicate-letters/duplicateLetters.js
+++ duplicate-letters/duplicateLetters.js
@@ -1,6 +1,17 @@
 
 function duplicateLetters(word) {
-  return args;
+  const letters = word.split('');
+  let Occurance = [];
+  const x = letters.map(( letter ) => {
+    if(!Occurance[letter] && isNaN(Occurance[letter]) ){
+      Occurance[letter] = 1;
+    }else{
+      Occurance[letter] += 1;
+    }  
+  }); 
+ 
+  const DuplicateLetterCount = Occurance.reduce((AddedValue, CurrentValue) => AddedValue + currentValue);
+  return DuplicateLetterCount;
 }
 
 export {
