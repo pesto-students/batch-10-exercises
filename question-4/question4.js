a3834d0d584007939df04ee743fb75036ff8d392
p-racoon <prasham9.ash@gmail.com>
2019-09-04 08:56:32 +0530

    completed question-4 Insert row

--- question-4/question4.js
+++ question-4/question4.js
@@ -1,8 +1,14 @@
-
-function question4(...args) {
-  return args;
+function insertRow() {
+  const messageLog = document.getElementsByClassName('logMessage')[0];
+  const table = document.getElementById('sampleTable');
+  const noOfRows = table.getElementsByTagName('tr').length;
+  const row = table.insertRow(-1);
+  for (let j = 0; j < 2; j += 1) {
+    const cell = document.createElement('td');
+    const cellText = document.createTextNode(`Row${noOfRows + 1} cell${j + 1}`);
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+  }
+  table.appendChild(row);
+  messageLog.innerHTML = 'New Row Added';
 }
-
-export {
-  question4,
-};
