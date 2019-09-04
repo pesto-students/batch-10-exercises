5fcce254f3456d34dbab74407e965c89475c0f19
p-racoon <prasham9.ash@gmail.com>
2019-09-04 23:01:35 +0530

    complete StopWatch

--- intermediate-exercises/src/components/02-StopWatch.js
+++ intermediate-exercises/src/components/02-StopWatch.js
@@ -1,26 +1,69 @@
-import React, { Component } from 'react';
-
-/*
-* Exercise 2:
-*
-*  Create a `StopWatch` component that has a Start/Stop button and a Clear
-*  button.
-*
-*  Pressing Start will start a timer and the lapsed time in
-*  milliseconds should be displayed above the buttons.
-*
-*  Once started the Start button should change to Stop. Clicking Stop
-*  will stop the timer but lapsed time will be preserved.
-*
-*  Clicking Start again will resume the timer from where it left off.
-*
-*  Clicking Clear will stop the timer if it's running and reset the lapsed time to 0.
-*/
+import React, { Component } from "react";
 
 class StopWatch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displaySeconds: 0,
+      displayMilliseconds: 0,
+      timerId: null
+    };
+  }
+  tick() {
+    let milliseconds = this.state.displayMilliseconds;
+    let seconds = this.state.displaySeconds;
+    milliseconds += 1;
+    if (milliseconds >= 60) {
+      seconds += 1;
+      milliseconds = 0;
+    }
+    this.setState({
+      displaySeconds: seconds,
+      displayMilliseconds: milliseconds
+    });
+  }
+  handleClear() {
+    clearInterval(this.state.timerId);
+    this.setState({ displaySeconds: 0, displayMilliseconds: 0, timerId: null });
+  }
+  handleStartStop() {
+    if (this.state.timerId) {
+      clearInterval(this.state.timerId);
+      this.setState({ timerId: null });
+    } else {
+      const intervalId = setInterval(() => this.tick(), 100);
+      this.setState({ timerId: intervalId });
+    }
+  }
   render() {
+    const formattedTime = `${this.state.displaySeconds
+      .toString()
+      .padStart(2, "0")} : ${this.state.displayMilliseconds
+      .toString()
+      .padStart(2, "0")}`;
     return (
-      <div>Stop Watch</div>
+      <div>
+        <h2>Stop Watch </h2>
+        <div className="stopWatch">
+          <time dateTime={formattedTime}>{formattedTime}</time>
+          <br />
+          <p>(sec : ms)</p>
+        </div>
+        <button
+          className="stopWatchButton"
+          onClick={() => this.handleStartStop()}
+          type="button"
+        >
+          {this.state.timerId ? "Stop" : "Start"}
+        </button>
+        <button
+          className="stopWatchButton"
+          onClick={() => this.handleClear()}
+          type="button"
+        >
+          Clear
+        </button>
+      </div>
     );
   }
 }
