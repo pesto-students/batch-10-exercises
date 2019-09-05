1a5bbe1c6783ebc4eb1217802022e23f7bff7853
p-racoon <prasham9.ash@gmail.com>
2019-09-05 09:42:47 +0530

    complete chunk array in groups

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,8 +1,16 @@
-
-function chunkArrayInGroups(...args) {
-  return args;
+function chunkArrayInGroups(arr, chunkSize) {
+  const chunkedGroup = [];
+  let sliceStartIndex = 0;
+  let sliceEndIndex = chunkSize;
+  while (sliceEndIndex < arr.length) {
+    const slicedChunk = arr.slice(sliceStartIndex, sliceEndIndex);
+    chunkedGroup.push(slicedChunk);
+    sliceStartIndex += chunkSize;
+    sliceEndIndex += chunkSize;
+  }
+  const lastChunk = arr.slice(sliceStartIndex, arr.length);
+  chunkedGroup.push(lastChunk);
+  return chunkedGroup;
 }
 
-export {
-  chunkArrayInGroups,
-};
+export { chunkArrayInGroups };
