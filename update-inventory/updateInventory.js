72cc17ed288cfb87b7f80d72ae8afebf9600f839
p-racoon <prasham9.ash@gmail.com>
2019-09-06 00:49:14 +0530

    complete updateinventory

--- update-inventory/updateInventory.js
+++ update-inventory/updateInventory.js
@@ -1,8 +1,19 @@
-
-function updateInventory(...args) {
-  return args;
+function updateInventory(firstArray, secondArray) {
+  const inventoryMap = new Map();
+  [...firstArray, ...secondArray].forEach(element => {
+    let value = element[0];
+    const keyString = element[1];
+    if (inventoryMap.has(keyString)) {
+      value += inventoryMap.get(keyString);
+    }
+    inventoryMap.set(keyString, value);
+  });
+  const outputArr = [];
+  for (const item of inventoryMap) {
+    outputArr.push([item[1], item[0]]);
+  }
+  outputArr.sort((a, b) => a[1] > b[1]);
+  return outputArr;
 }
 
-export {
-  updateInventory,
-};
+export { updateInventory };
