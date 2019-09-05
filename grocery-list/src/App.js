2656e7138942dbadccaa694bc110b19a5aac8fdf
thapasya-m <thapasya.murali@gmail.com>
2019-09-05 20:06:13 +0530

    grocery - add, clear wt hooks

--- grocery-list/src/App.js
+++ grocery-list/src/App.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import './App.css';
+import './styles/App.css';
 
-const App = () => (
-  <div>App</div>
-);
+function App() {
+  const [items, setItem] = useState(['milk']);
+  const [input, setInput] = useState(null);
+
+  function addItem() {
+    setItem([...items, input]);
+    setInput('');
+  }
+
+  useEffect(() => {
+    // addItem();
+
+    // return () => {
+    //   ChatAPI.unsubscribeFromFriendStatus(props.friend.id, handleStatusChange);
+    // };
+  }, [items]);
+  // eslint-disable-next-line react/jsx-filename-extension
+  return (
+    // eslint-disable-next-line react/jsx-filename-extension
+    <div>
+      <ul>
+        {items.map(item => <li>{item}</li>)}
+      </ul>
+      <form>
+        <label htmlFor="item">
+          item:
+          <input
+            type="text"
+            name="item"
+            value={input}
+            onChange={({ target: { value } }) => setInput(value)}
+          />
+        </label>
+        <button type="button" onClick={addItem}>Add</button>
+        <button type="button" onClick={() => setItem([])}>Clear</button>
+      </form>
+    </div>
+  );
+}
 
 export default App;
