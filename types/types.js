fff3f4206ea8b032a9b7d88c006175306331d441
ArpitKumar2092 <arpitkumar2092@gmail.com>
2019-09-06 01:03:08 +0530

    Working On Types

--- types/types.js
+++ types/types.js
@@ -1,6 +1,14 @@
 
-function types(...args) {
-  return args;
+function types(input) {
+  if (Array.isArray(element)) {
+    return 'Array';
+  } else if (element instanceof Object) {
+    return 'Object';
+  } else if (element instanceof RegExp) {
+    return 'RegExp';
+  } else if (element instanceof Function) {
+    return 'Function';
+  } 
 }
 
 export {
