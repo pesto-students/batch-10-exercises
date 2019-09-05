1cd8bb640ca6b72fdb1e3237f1e837f3943bdfdb
ArpitKumar2092 <arpitkumar2092@gmail.com>
2019-09-05 22:37:33 +0530

    Completed Factorial.js

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,8 +1,15 @@
+function factorial(number) {
+  if(typeof number != 'number'){
+    throw "Expected a input of type number. You entered ${typeof number}";
+  }
 
-function factorial(...args) {
-  return args;
+  if (number == 0 || number == 1 ) {
+    return 1;
+  }
+
+  return number * factorial(number-1);
 }
 
 export {
-  factorial,
-};
+  factorial
+}
