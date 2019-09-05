5b317f95c9dc9aeef06adaa35f81c1004a719b6a
p-racoon <prasham9.ash@gmail.com>
2019-09-05 16:03:09 +0530

    incomplete smallest Common

--- smallest-commons/smallestCommons.js
+++ smallest-commons/smallestCommons.js
@@ -1,8 +1,16 @@
-
-function smallestCommons(...args) {
-  return args;
+function smallestCommons(numArr) {
+  const gcd = (a, b) => {
+    if (b == 0) return a;
+    return gcd(a, b % a);
+  };
+  const lcm = () => (num1 * num2) / gcd(num1, num2);
+  const num1 = Math.min(numArr[0], numArr[1]);
+  const num2 = Math.max(numArr[0], numArr[1]);
+  let currentLcm = num1;
+  while (num1 < num2) {
+    currentLcm = lcm(currentLcm, num1);
+  }
+  return currentLcm;
 }
 
-export {
-  smallestCommons,
-};
+export { smallestCommons };
