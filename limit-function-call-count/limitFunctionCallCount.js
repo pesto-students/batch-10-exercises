02ba44a11ed8c5caf1a09c183821281d920d8c8e
ArpitKumar2092 <arpitkumar2092@gmail.com>
2019-09-05 22:47:48 +0530

    Completed limitFunctionCallCount

--- limit-function-call-count/limitFunctionCallCount.js
+++ limit-function-call-count/limitFunctionCallCount.js
@@ -1,6 +1,14 @@
 
-function limitFunctionCallCount(...args) {
-  return args;
+function limitFunctionCallCount(Fn , AllowedCall) {
+  let callCounter = 1;
+  return (...inputs) =>{
+    if(callCounter <= AllowedCall){
+      callCounter++;
+      return Fn( ...inputs );
+    }else{
+      return null;
+    }
+  }
 }
 
 export {
