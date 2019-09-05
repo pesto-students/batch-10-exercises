9635a1fc7a60166ceff2a2e2d2984f28dae6ddef
p-racoon <prasham9.ash@gmail.com>
2019-09-05 23:53:14 +0530

    complete Grocery List app

--- grocery-list/src/App.js
+++ grocery-list/src/App.js
@@ -1,7 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
-const App = () => <div>App</div>;
+const AddGroceryForm = ({ addToGroceryList }) => {
+  const [input, setInput] = useState("");
+  return (
+    <div>
+      <label htmlFor="addItem">Add new item - </label>
+      <input
+        type="text"
+        required={true}
+        value={input}
+        onChange={e => setInput(e.target.value)}
+      />
+      <button class="btn btn-primary" onClick={() => addToGroceryList(input)}>
+        Add +
+      </button>
+    </div>
+  );
+};
+
+const GroceryItem = ({ item, removeItem }) => {
+  const [checked, setChecked] = useState(false);
+  return (
+    <div
+      className={checked ? "itemChecked" : "itemUnchecked"}
+      onClick={() => setChecked(!checked)}
+    >
+      <p>
+        {item}
+        <button
+          className="deleteButton btn btn-link"
+          onClick={() => removeItem(item)}
+        >
+          X
+        </button>
+      </p>
+    </div>
+  );
+};
+const GroceryList = ({ groceryList, removeItem }) => {
+  return (
+    <ul>
+      {groceryList.map(item => (
+        <li key={item}>
+          <GroceryItem item={item} removeItem={removeItem} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const App = () => {
+  const useGroceryList = initialState => {
+    const [items, setItems] = useState(initialState);
+    return [
+      items,
+      item => {
+        const newArr = [...items];
+        newArr.push(item);
+        setItems(newArr);
+      },
+      itemToRemove => {
+        setItems(items.filter(el => el !== itemToRemove));
+      }
+    ];
+  };
+  const [groceryList, addToGroceryList, removeItem] = useGroceryList([]);
+  return (
+    <div className="app">
+      <h1>Teri-Meri Grocery</h1>
+      <AddGroceryForm addToGroceryList={addToGroceryList} />
+      <GroceryList groceryList={groceryList} removeItem={removeItem} />
+    </div>
+  );
+};
 
 export default App;
