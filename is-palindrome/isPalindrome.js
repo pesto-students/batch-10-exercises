c0a97e76ce1f229aa3d323592b80dea8d5773717
ArpitKumar2092 <arpitkumar2092@gmail.com>
2019-09-05 23:36:12 +0530

    Completed Sum-all

--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,6 +1,19 @@
 
-function isPalindrome(...args) {
-  return args;
+function isPalindrome(string) {
+  if(!isNaN){
+    string = String(string);
+  }
+
+  const RegexToGetOnlyLetterAndNumber = '/[^a-z0-9]+/g';
+
+  string = string.replace(RegexToGetOnlyLetterAndNumber,'').toLowerCase();
+  const reversedString = string.split('').reverse().join('');
+  if(string == reversedString){
+    return true;
+  }
+  console.log(reversedString , string)
+
+  return false;
 }
 
 export {
