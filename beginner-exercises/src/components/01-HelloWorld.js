031b8d28cc86219353502047e1d2dd8baeb8026c
thapasya-m <thapasya.murali@gmail.com>
2019-09-05 23:56:40 +0530

    bgnr exer - 1, 3

--- beginner-exercises/src/components/01-HelloWorld.js
+++ beginner-exercises/src/components/01-HelloWorld.js
@@ -1,14 +1,7 @@
 import React, { Component } from 'react';
 
-// Task: Render "Hello World" text.
 // eslint-disable-next-line react/prefer-stateless-function
 class HelloWorld extends Component {
-  // All components *must* have a `render` method defined.
-
-  // To define a component's render method, we use syntax called JSX. As you
-  // can see it looks similar to HTML. JSX gets converted to
-  // JavaScript code. It is here just for readability purposes.
-
   render() {
     const text = 'hello world!';
     return (
--- beginner-exercises/src/components/03-FavoriteMovie.js
+++ beginner-exercises/src/components/03-FavoriteMovie.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class FavoriteMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = { movie: 'Hey there. Enter your favorite movie.' };
+    this.state = { movie: '' };
     this.onMovieChange = this.onMovieChange.bind(this);
   }
 
@@ -11,7 +11,7 @@ class FavoriteMovie extends Component {
   onMovieChange(event) {
     const input = event.target;
     this.setState({
-      movie: input.value.length > 0 ? input.value : 'Hey there. Enter your favorite movie.',
+      movie: input.value,
     });
   }
 
@@ -20,14 +20,23 @@ class FavoriteMovie extends Component {
     return (
       // eslint-disable-next-line react/jsx-filename-extension
       <div>
-        <p>
-          My favorite movie is
-          <span style={{ color: 'blue' }}>{movie}</span>
-        </p>
+        {(movie === '')
+          ?
+          <p>
+            Hey there. Enter your favorite movie.
+          </p>
+          :
+          <p>
+            My favorite movie is
+            <span style={{ color: 'blue' }}>
+            {movie}
+            </span>
+          </p>
+        }
         <form>
           <label htmlFor="movie">
-            <input type="text" id="user-movie" name="movie" onChange={this.onMovieChange} />
             movie:
+            <input type="text" id="user-movie" name="movie" onChange={this.onMovieChange} />
           </label>
         </form>
       </div>
