926c600780528ead1d05b830bc406015384982a6
p-racoon <prasham9.ash@gmail.com>
2019-09-06 16:07:15 +0530

    feat and fix reactRouterDom Nav Usage

--- setup-router/src/App.js
+++ setup-router/src/App.js
@@ -1,21 +1,27 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import NoMatch from "./components/NoMatch";
 
-import './styles/App.css';
+import "./styles/App.css";
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
+      <Router>
         <Navbar />
-        <Home />
-        <About />
-        <Contact />
-      </Fragment>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route component={NoMatch} />
+        </Switch>
+      </Router>
     );
   }
 }
--- setup-router/src/components/Navbar.js
+++ setup-router/src/components/Navbar.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
-import '../styles/App.css';
+import "../styles/App.css";
 
 export default class Navbar extends Component {
   render() {
     return (
-      <ul className="nav">
-        <li className="active">Home</li>
-        <li>About Us</li>
-        <li>Contact</li>
-      </ul>
+      <nav className="nav">
+        <NavLink to="/home" activeClassName="active">
+          Home
+        </NavLink>
+        <NavLink to="/about" activeClassName="active">
+          About Us
+        </NavLink>
+        <NavLink to="/contact" activeClassName="active">
+          Contact
+        </NavLink>
+      </nav>
     );
   }
 }
