3fa2c70b4d354d2676cd29a0c1deacb8ce10bcfb
thapasya-m <thapasya.murali@gmail.com>
2019-09-06 16:25:28 +0530

    merge all

--- merge-all/mergeAll.js
+++ merge-all/mergeAll.js
@@ -1,6 +1,14 @@
 
-function mergeAll(...args) {
-  return args;
+function mergeAll(args) {
+  const object = {};
+  args.forEach((obj) => {
+    for (const key in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        object[key] = obj[key];
+      }
+    }
+  });
+  return object;
 }
 
 export {
