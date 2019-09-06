126c081f1e9dd4f7c073236d198f72107c33437f
p-racoon <prasham9.ash@gmail.com>
2019-09-06 16:30:24 +0530

    chore and fix  mergeAll

--- merge-all/mergeAll.js
+++ merge-all/mergeAll.js
@@ -1,8 +1,13 @@
-
-function mergeAll(...args) {
-  return args;
+function mergeAll(objectsList) {
+  const mergedObj = {};
+  objectsList.forEach(obj => {
+    for (const key in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        mergedObj[key] = obj[key];
+      }
+    }
+  });
+  return mergedObj;
 }
 
-export {
-  mergeAll,
-};
+export { mergeAll };
