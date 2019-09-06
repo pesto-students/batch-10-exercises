093426115e35c75b492d7b58d6cacbd480c13d89
p-racoon <prasham9.ash@gmail.com>
2019-09-06 20:30:00 +0530

    incomplete colorful-router

--- colorful-router/src/components/Color.jsx
+++ colorful-router/src/components/Color.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-import '../styles/Color.css';
+import "../styles/Color.css";
 
-const Color = (props) => {
-  const { color } = props;
+const Color = props => {
+  const { color } = props.match.params.color;
   return (
     <div className="Color" style={{ backgroundColor: color.hex }}>
       <p>this is {color.name}.</p>
       <p>Isnt it beautiful?</p>
-      <p><Link to="/">Go back</Link></p>
+      <p>
+        <Link to="/">Go back</Link>
+      </p>
     </div>
   );
 };
@@ -18,8 +20,8 @@ const Color = (props) => {
 Color.propTypes = {
   color: PropTypes.shape({
     hex: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-  }).isRequired,
+    name: PropTypes.string.isRequired
+  }).isRequired
 };
 
 export default Color;
