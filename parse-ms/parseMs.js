251e8a501a93067af8cc2d1ec6ce6274ee619e5e
ArpitKumar2092 <arpitkumar2092@gmail.com>
2019-09-06 11:43:40 +0530

    working on  parseMs

--- parse-ms/parseMs.js
+++ parse-ms/parseMs.js
@@ -1,6 +1,17 @@
 
-function parseMs(...args) {
-  return args;
+function parseMs(ms) {
+  const CheckForNegative = String(ms).split('-').length; 
+  if(ms <= 0 || CheckForNegative == 2){
+    return ms;
+  }
+  const ParsedMS = new Date(ms).getMilliseconds();
+  return {
+    days: (ms / (1000 * 60 * 60 * 24)),
+    hours: (ms / (1000 * 60 * 60)),
+    minutes: (ms / (1000 * 60)),
+    seconds: (ms / (1000)),
+    milliseconds: ParsedMS,
+  }
 }
 
 export {
