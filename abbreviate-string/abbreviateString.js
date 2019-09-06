275f182c6335461f3777e8a2de64ca57115b9e92
p-racoon <prasham9.ash@gmail.com>
2019-09-06 10:39:39 +0530

    completed abbreviated string

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,8 +1,15 @@
-
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(inputStr) {
+  if (typeof inputStr !== "string") {
+    throw new Error(
+      `Invalid Input. Expected String, received ${typeof inputStr}`
+    );
+  }
+  const splitString = inputStr.split(" ");
+  const lastIndex = splitString.length - 1;
+  const abbreviatedStr = `${splitString[0]} ${splitString[
+    lastIndex
+  ][0].toUpperCase()}.`;
+  return abbreviatedStr;
 }
 
-export {
-  abbreviateString,
-};
+export { abbreviateString };
