ddbb03c6ae910dbb5e7b5d68bdbf3b835e883a4a
ArpitKumar2092 <arpitkumar2092@gmail.com>
2019-09-06 23:53:09 +0530

    Working On colorful-router

--- colorful-router/src/components/ColorList.js
+++ colorful-router/src/components/ColorList.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { BrowserRouter, Switch, Route, Redirect, Link } from 'react-router-dom';
 
 import '../styles/ColorList.css';
 
 const ColorList = (props) => {
   const colorLinks = props.colors.map(color => (
-    <li key={color.hex}>
+    <li key={color.hex}><Link to={color.name} >
       {color.name}
+      </Link>
     </li>
   ));
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="App-title">Welcome to the Colorful Router.</h1>
-        <h1>Add a color</h1>
+        <h1><Link to={`new`} >Add a color </Link></h1>
       </header>
       <div className="App-intro">
         <p>Please select a color.</p>
