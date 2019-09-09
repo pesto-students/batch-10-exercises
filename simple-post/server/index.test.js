7c367b32fe05520001710dd96b8d54c6bcda9ad9
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-09-09 20:06:10 +0530

    implemented authentication tests for server

--- simple-post/server/index.test.js
+++ simple-post/server/index.test.js
@@ -2,19 +2,61 @@ const fetch = require('node-fetch');
 
 const BASE_URL = 'http://localhost:3001';
 
-// const retrieveStatus = async url => {
-//   const res = await fetch(url);
-//   return res;
-// };
-
-// test('Responds with status code:200', () => {
-//    expect(fetch(BASE_URL).then(res => res.status)).toBe(200)
-// }
-
 describe('BASE_URL', () => {
   test('Responds with status code:200', async () => {
     const response = await fetch(BASE_URL);
     const status = await response.status;
     expect(status).toBe(200);
+  });
+  test("Responds with text `Hello. What's up?`", async () => {
+    const response = await fetch(BASE_URL);
+    const text = await response.text();
+    expect(text).toBe("Hello. What's up?");
+  });
+});
+
+describe('BASE_URL/posts/', () => {
+  test('Responds with status code:403 if no password is sent', async () => {
+    const response = await fetch(BASE_URL + '/posts');
+    const status = await response.status;
+    expect(status).toBe(403);
+  });
+  test('Responds with status code:403 if wrong password is sent', async () => {
+    const response = await fetch(BASE_URL + '/posts', {
+      headers: {
+        'pesto-password': 'darth vade'
+      }
+    });
+    const status = await response.status;
+    expect(status).toBe(403);
+  });
+  test('Responds with status code:200 if right password is sent', async () => {
+    const response = await fetch(BASE_URL + '/posts', {
+      headers: {
+        'pesto-password': 'darth vader'
+      }
+    });
+    const status = await response.status;
+    expect(status).toBe(200);
+  });
+  test('Responds with status code:403 if wrong header is sent for password', async () => {
+    const response = await fetch(BASE_URL + '/posts', {
+      headers: {
+        'pesto-passwor': 'darth vader'
+      }
+    });
+    const status = await response.status;
+    expect(status).toBe(403);
+  });
+  test('Responds with message:`No pesto-password header present. Hint: password is `darth vader`` if authentication credentials are incorrect', async () => {
+    const response = await fetch(BASE_URL + '/posts', {
+      headers: {
+        'pesto-passwor': 'darth vader'
+      }
+    });
+    const json = await response.json();
+    expect(json.error.message).toBe(
+      'No pesto-password header present. Hint: password is `darth vader`'
+    );
   });
 });
