109746f0ba515e7239f610dfc8104a13204690dd
thapasya <thapasya.murali@gmail.com>
2019-09-10 01:54:53 +0530

    update obj

--- update-object/updateObject.js
+++ update-object/updateObject.js
@@ -1,6 +1,20 @@
 
-function updateObject(...args) {
-  return args;
+function updateObject(idx, val, arr) {
+  return (function (value) {
+    return function (array) {
+      const arrLen = array.length;
+      idx = (idx + arrLen) % arrLen;
+      array[idx] = value;
+      return array;
+    };
+  }(val))(arr);
+  // const arrLen = arr.length;
+  // return (function (inputArray) {
+  //   inputArray[(index + arrLen) % arrLen] = value;
+  //   return inputArray;
+  // }(arr));
+  // inputArray[(index + arrLen) % arrLen] = value;
+  // return inputArray;
 }
 
 export {
