6ee0fe48f34e996bb30a7186af7d3d80a3fbddcb
thapasya <thapasya.murali@gmail.com>
2019-09-09 16:54:26 +0530

    fetched data &displayed in post comp

--- simple-post/frontend/src/App.js
+++ simple-post/frontend/src/App.js
@@ -6,7 +6,55 @@ import './App.css';
 class App extends Component {
   state = {
     posts: [],
+    url: "http://localhost:3001/posts/",
+    page: 1,
   };
+
+  getPosts() {
+    const url = this.state.url + this.state.page;
+    fetch(url, {
+      method: 'GET',
+      headers:{
+        'Content-Type': 'application/json',
+        'pesto-password': 'darth vader'
+      }
+    })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      const response = JSON.stringify(myJson);
+      console.log(response);
+      this.setState({
+        posts: myJson.data
+      })
+    });
+  }
+
+  componentDidMount() {
+    const url = this.state.url + this.state.page;
+    fetch(url, {
+      method: 'GET',
+      headers:{
+        'Content-Type': 'application/json',
+        'pesto-password': 'darth vader'
+      }
+    })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            posts: result.data
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          console.log(error);
+        }
+      )
+  }
 
   render() {
     return (
