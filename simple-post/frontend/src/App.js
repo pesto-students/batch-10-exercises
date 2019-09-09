65d7a49f3c1b9779e41ab958d557590716c9ad1d
thapasya <thapasya.murali@gmail.com>
2019-09-09 19:00:56 +0530

    implemented get post, paginated

--- simple-post/frontend/src/App.js
+++ simple-post/frontend/src/App.js
@@ -4,60 +4,59 @@ import Post from './components/Post';
 import './App.css';
 
 class App extends Component {
-  state = {
-    posts: [],
-    url: "http://localhost:3001/posts/",
-    page: 1,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      url: 'http://localhost:3001/posts/',
+      page: 1,
+    };
+    this.handlePrevClick = this.handlePrevClick.bind(this);
+    this.handleNextClick = this.handleNextClick.bind(this);
+  }
+  componentDidMount() {
+    this.getPosts();
+  }
 
   getPosts() {
     const url = this.state.url + this.state.page;
     fetch(url, {
       method: 'GET',
-      headers:{
+      headers: {
         'Content-Type': 'application/json',
-        'pesto-password': 'darth vader'
-      }
-    })
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(myJson) {
-      const response = JSON.stringify(myJson);
-      console.log(response);
-      this.setState({
-        posts: myJson.data
-      })
-    });
-  }
-
-  componentDidMount() {
-    const url = this.state.url + this.state.page;
-    fetch(url, {
-      method: 'GET',
-      headers:{
-        'Content-Type': 'application/json',
-        'pesto-password': 'darth vader'
-      }
+        'pesto-password': 'darth vader',
+      },
     })
       .then(res => res.json())
       .then(
         (result) => {
           this.setState({
-            posts: result.data
+            posts: result.data,
           });
         },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
         (error) => {
           console.log(error);
-        }
-      )
+        },
+      );
+  }
+
+  handlePrevClick() {
+    this.setState(( state ) => {
+      return { page: state.page === 1 ? 1 : state.page - 1 };
+    }, () => {
+      this.getPosts();
+    });
+  }
+
+  handleNextClick() {
+    this.setState(state => ({ page: state.page === 10 ? 10 : state.page + 1 }), () => {
+      this.getPosts();
+    });
   }
 
   render() {
     return (
+      // eslint-disable-next-line react/jsx-filename-extension
       <div>
         <h2>Posts</h2>
         <hr />
