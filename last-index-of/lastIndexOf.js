670eabf7d8e436e24d5806ab4834e70c58924a9f
thapasya <thapasya.murali@gmail.com>
2019-09-10 01:24:40 +0530

    last index

--- last-index-of/lastIndexOf.js
+++ last-index-of/lastIndexOf.js
@@ -1,6 +1,11 @@
 
-function lastIndexOf(...args) {
-  return args;
+function lastIndexOf(number, args) {
+  for (let index = args.length - 1; index > -1; index -= 1) {
+    if (args[index] === number) {
+      return index;
+    }
+  }
+  return -1;
 }
 
 export {
