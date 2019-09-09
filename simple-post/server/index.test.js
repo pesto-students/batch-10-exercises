83dd59cadbc2c67260f5cd0fa21bf2661db4f2cb
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-09-09 19:50:05 +0530

    Response test

--- simple-post/server/index.test.js
+++ simple-post/server/index.test.js
@@ -1,5 +1,20 @@
-describe('Sample Test', () => {
-  it('should test that true === true', () => {
-    expect(true).toBe(true);
+const fetch = require('node-fetch');
+
+const BASE_URL = 'http://localhost:3001';
+
+// const retrieveStatus = async url => {
+//   const res = await fetch(url);
+//   return res;
+// };
+
+// test('Responds with status code:200', () => {
+//    expect(fetch(BASE_URL).then(res => res.status)).toBe(200)
+// }
+
+describe('BASE_URL', () => {
+  test('Responds with status code:200', async () => {
+    const response = await fetch(BASE_URL);
+    const status = await response.status;
+    expect(status).toBe(200);
   });
 });
