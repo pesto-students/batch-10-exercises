804e43fd4626df4edc8abafe06803b81a585556a
thapasya <thapasya.murali@gmail.com>
2019-09-09 16:36:05 +0530

    initial commit

--- simple-post/frontend/src/components/Post.js
+++ simple-post/frontend/src/components/Post.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Post = ({ post }) => {
-  return (
+const Post = ({ post }) => (
     <div key={post.id}>
       <h4>{post.id}</h4>
       <div>Title: {post.title}</div>
     </div>
   );
-};
 
 Post.propTypes = {
   post: PropTypes.shape({
