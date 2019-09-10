ae000aef4ad7f2d3a8e5ea27d3d20885692c652b
thapasya <thapasya.murali@gmail.com>
2019-09-10 16:32:49 +0530

    file filter#2

--- filtered-names-recursive/filteredNamesRecursive.js
+++ filtered-names-recursive/filteredNamesRecursive.js
@@ -2,7 +2,19 @@ const fs = require('fs');
 const path = require('path');
 
 function filteredNamesRecursive(pathname, type) {
-  return args;
+  const fileArr = [];
+  const recursiveRead = (dir, filelist) => {
+    const files = fs.readdirSync(dir);
+    files.forEach((file) => {
+      if (path.extname(file) === '') {
+        filelist = recursiveRead(`${dir}/${file}/`, filelist);
+      } else if (path.extname(file) === `.${type}`) {
+        filelist.push(file);
+      }
+    });
+    return filelist;
+  };
+  return recursiveRead(pathname, fileArr);
 }
 
 export {
--- filtered-names/filteredNames.js
+++ filtered-names/filteredNames.js
@@ -1,6 +1,14 @@
+const fs = require('fs');
+const path = require('path');
 
-function filteredNames(...args) {
-  return args;
+function filteredNames(pathname, type) {
+  const outFiles = [];
+  fs.readdirSync(pathname).forEach((file) => {
+    if (path.extname(file) === `.${type}`) {
+      outFiles.push(file);
+    }
+  });
+  return outFiles;
 }
 
 export {
