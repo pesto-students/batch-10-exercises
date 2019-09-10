eb037ab8e9fb8fafc3c86a48d63a191467d4037b
p-racoon <prasham9.ash@gmail.com>
2019-09-10 16:54:58 +0530

    complete jsonresponseServer

--- json-response-server/jsonResponseServer.js
+++ json-response-server/jsonResponseServer.js
@@ -1,8 +1,13 @@
+const http = require("http");
 
-function jsonResponseServer(...args) {
-  return args;
-}
-
-export {
-  jsonResponseServer,
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ data: "Pesto Bootcamp!" }));
+});
+const jsonResponseServer = {
+  listen: (port, host = "127.0.0.1") => server.listen(port, host),
+  close: () => server.close()
 };
+
+export { jsonResponseServer };
