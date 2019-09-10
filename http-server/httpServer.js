656b6999c86900234f6c82eb4a2102e6c5635aa9
ArpitKumar2092 <arpitkumar2092@gmail.com>
2019-09-10 16:07:11 +0530

    Completed HttpServer

--- http-server/httpServer.js
+++ http-server/httpServer.js
@@ -1,7 +1,29 @@
 
-function httpServer(...args) {
-  return args;
+const http = require('http');
+
+
+let app = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Pesto Bootcamp!\n');
+});
+
+
+const httpServer =  {
+  listen :  function(PortNumber){
+    app.listen(PortNumber, '127.0.0.1');
+  },
+  close :  function(){
+    app.close();
+  }
 }
+
+
+
+
+
+app.listen(3000, '127.0.0.1');
+
+
 
 export {
   httpServer,
