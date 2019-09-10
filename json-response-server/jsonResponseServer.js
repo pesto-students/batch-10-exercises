dbb32286c74ab12c79ed0eaf9f3c4e1174a143c8
thapasya <thapasya.murali@gmail.com>
2019-09-10 16:48:44 +0530

    json resp srvr

--- json-response-server/jsonResponseServer.js
+++ json-response-server/jsonResponseServer.js
@@ -1,7 +1,10 @@
+const express = require('express');
 
-function jsonResponseServer(...args) {
-  return args;
-}
+const jsonResponseServer = express();
+
+jsonResponseServer.get('/', (req, response) => {
+  response.status(200).send({ data: 'Pesto Bootcamp!' });
+});
 
 export {
   jsonResponseServer,
