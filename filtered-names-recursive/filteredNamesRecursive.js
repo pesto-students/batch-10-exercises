4e6f0711cb944f759d1d5268f9f433f0c6697623
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-09-10 16:12:52 +0530

    filtered name recursive

--- filtered-names-recursive/filteredNamesRecursive.js
+++ filtered-names-recursive/filteredNamesRecursive.js
@@ -1,8 +1,21 @@
+const fs = require('fs');
+const path = require('path');
 
 function filteredNamesRecursive(...args) {
-  return args;
+  const [directory, extension] = [...args];
+
+  const walkSync = (dir, filelist = []) => {
+    const files = fs.readdirSync(dir);
+    files.forEach(file => {
+      if (fs.statSync(dir + '/' + file).isDirectory()) {
+        filelist = walkSync(dir + '/' + file, filelist);
+      } else {
+        filelist.push(file);
+      }
+    });
+    return filelist;
+  };
+  return walkSync(directory).filter(file => file.endsWith('.json'));
 }
 
-export {
-  filteredNamesRecursive,
-};
+export { filteredNamesRecursive };
