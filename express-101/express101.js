6d6fb953c32d69f4acd84a0cc9cc7b3863665c16
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-09-11 16:33:21 +0530

    express 101-complete

--- express-101/express101.js
+++ express-101/express101.js
@@ -1,5 +1,12 @@
-function express101(...args) {
-	return args;
-}
+const express = require('express');
 
-export { express101 };
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hey, I am server response'));
+
+app.get('/movie/:title', (req, res) => {
+	res.send(`My favorite movie is ${req.params.title}`);
+});
+
+app.listen(port);
