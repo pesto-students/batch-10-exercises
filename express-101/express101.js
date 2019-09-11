e6d77f350edd35bcbf345547036d4a277b875da2
thapasya <thapasya.murali@gmail.com>
2019-09-11 17:01:44 +0530

    express101, test

--- express-101/express101.js
+++ express-101/express101.js
@@ -8,10 +8,11 @@ express101.get('/', (req, res) => {
   res.send('Hey, I am server response');
 });
 
+express101.get('/movie/:title', (req, res) => {
+  const { title } = req.params;
+  res.send(`My favorite movie is ${title}`);
+});
+
 express101.listen(port, () => {
   console.log(`Server listening on port: ${port}`);
 });
-
-export {
-  express101,
-};
