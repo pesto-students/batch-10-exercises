45b6d0130b858e301c26394f7cd0c4839a307bb8
thapasya <thapasya.murali@gmail.com>
2019-09-11 16:16:30 +0530

    express101 - wrong branch revert changes

--- express-101/express101.js
+++ express-101/express101.js
@@ -1,16 +1,7 @@
-const express = require('express');
 
-const express101 = express();
-
-const port = 3000;
-
-express101.get('/', (req, res) => {
-  res.send('Hey, I am server response');
-});
-
-express101.listen(port, () => {
-  console.log(`Server listening on port: ${port}`);
-});
+function express101(...args) {
+  return args;
+}
 
 export {
   express101,
