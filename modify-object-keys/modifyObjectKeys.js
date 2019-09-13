52c79df63cfdd9c1e4d9d6a6271d491b0708c990
p-racoon <prasham9.ash@gmail.com>
2019-09-13 19:43:42 +0530

    complete modifyObjectKeys

--- modify-object-keys/modifyObjectKeys.js
+++ modify-object-keys/modifyObjectKeys.js
@@ -1,5 +1,12 @@
-function modifyObjectKeys(...args) {
-  return args;
+function modifyObjectKeys(obj, modifierFunc) {
+  const newObj = {};
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      const modifiedKey = modifierFunc(key);
+      newObj[modifiedKey] = obj[key];
+    }
+  }
+  return newObj;
 }
 
 export { modifyObjectKeys };
