ef8a277df443956a6b3502f5609bd57137ee5edb
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-09-13 19:44:05 +0530

    modify object keys

--- modify-object-keys/modifyObjectKeys.js
+++ modify-object-keys/modifyObjectKeys.js
@@ -1,8 +1,14 @@
-
 function modifyObjectKeys(...args) {
-  return args;
+  const [o, fn] = [...args];
+  const keys = Object.entries(o);
+
+  const newobj = {};
+  for (const key of keys) {
+    const newKey = fn(key[0]);
+    newobj[newKey] = key[1];
+  }
+
+  return newobj;
 }
 
-export {
-  modifyObjectKeys,
-};
+export { modifyObjectKeys };
